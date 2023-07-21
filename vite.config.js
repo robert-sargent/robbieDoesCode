be2a66b948eccd5c6fb0b4bf1a0dc039dbfc4a2b
Robert Sargent <blueshiftwebdev@icloud.com>
2023-07-20 22:00:32 -0700

    assets loaded

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "/pages/about.html"),
+        project: resolve(__dirname, "/pages/project.html"),
+      },
+    },
+  },
+});
